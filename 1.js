ee4e982161905d6be8020ac77ce9dc2365a929ab
Samrat Ghosh <samratghosh490@gmail.com>
2022-12-30 19:31:59 +0530

    2nd changes

--- 1.js
+++ 1.js
@@ -1,3 +1,18 @@
-console.log(2+3);
-var str="samrat";
-console.log(str);
+//create and initialize a global variable
+var num1 = 30;
+
+// defination of an function 
+function fn1(){
+
+    // create and initialize a local variable
+    var name = "samrat ghosh";
+    var num2=70;
+
+    console.log(name);
+    console.log(num1+num2);
+
+}
+
+
+// call the fn2 function 
+fn1();
